8ceea828c6765e75464ff6191611453d534345b9
Hazrat Ali <hazrat8143@gmail.com>
2024-04-22 23:29:36 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Auth from "./Auth";
+import authContext from "./Authcontext";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  //using the state to dynamicallly pass the values to the context
 
-export default App
+  const [authstatus, setauthstatus] = useState(false);
+  const login = () => {
+    setauthstatus(true);
+    // ckncnvjekf
+  };
+  return (
+    <React.Fragment>
+      <authContext.Provider value={{ status: authstatus, login: login }}>
+        <Auth />
+      </authContext.Provider>
+    </React.Fragment>
+  );
+};
+export default App;
